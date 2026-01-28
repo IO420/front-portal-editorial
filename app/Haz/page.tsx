72b66f6e52a4d00c30d6660c14d3941690a0bef2
frcarlos <307100636@pcpuma.acatlan.unam.mx>
2026-01-28 18:23:04 -0500

    Primer Axios

--- app/Haz/page.tsx
+++ app/Haz/page.tsx
@@ -1,4 +1,32 @@
-import './peticion.js';
-export default function Page() {
-  return <h1>Hi sr</h1>;
+'use client';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function Lange() {
+  const [users, setUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        const response = await axios.get(
+          'https://rickandmortyapi.com/api/character?page=1'
+        );
+        setUsers(response.data.results);
+        console.log(response.data.results[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getCharacters();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hi</h1>
+
+      {users.length > 0 && <p>{users[0].name}</p>}
+      <img src={users[0].image} alt="" width={50} />
+    </div>
+  );
 }
---
+++ app/Haz/peticion.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { useEffect } from 'react';
+
+export default function Axios() {
+  useEffect(() => {
+    console.log('hola');
+  }, []);
+
+  return <></>;
+}
